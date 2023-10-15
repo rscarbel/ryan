cffd6f5f4a9627f5e45a3c59ac2cd1a4ee9e7f71
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2023-10-15 15:40:28 -0400

    adding this even though it doesn't work on my mac

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image'
+import { getContentByKey } from "@/lib/siteContent";
+import ryansvg from "./ryansvg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>
+        {(await getContentByKey("SITE_HEADLINE")) || "Site Headline goes here"}
+      </h1>
+      <h3>{(await getContentByKey("SITE_SUBTITLE")) || ""} </h3>
+      {ryansvg}
+    </main>
   );
 }
