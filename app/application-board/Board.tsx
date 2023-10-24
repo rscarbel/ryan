cb81aa87a40975f68c75b7a4d4a1d5f85903630b
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2023-10-24 17:59:36 -0400

    destructured props

--- app/application-board/Board.tsx
+++ app/application-board/Board.tsx
@@ -13,6 +13,7 @@ import DoubleColumn from "./DoubleColumn";
 
 const Board: React.FC = ({ cards = [] }) => {
   const [boardData, setBoardData] = useState(initializeBoardData(cards));
+  const { columns, applicationCards, columnOrder } = boardData;
 
   const onDragEnd = (result) => {
     const { source, destination, draggableId } = result;
@@ -25,13 +26,13 @@ const Board: React.FC = ({ cards = [] }) => {
       return;
     }
 
-    const startColumn = boardData.columns[source.droppableId];
+    const startColumn = columns[source.droppableId];
     const updatedColumns =
       source.droppableId === destination.droppableId
         ? handleSameColumnMove(startColumn, source, destination, draggableId)
         : handleDifferentColumnMove(
             startColumn,
-            boardData.columns[destination.droppableId],
+            columns[destination.droppableId],
             source,
             destination,
             draggableId
@@ -48,46 +49,48 @@ const Board: React.FC = ({ cards = [] }) => {
       offer: "accepted",
       rejected: "passed",
     };
-    if (doubleColumns[columnId]) {
-      const column = boardData.columns[columnId];
-      const pairedColumn = boardData.columns[doubleColumns[columnId]];
 
+    if (doubleColumns[columnId]) {
+      const column = columns[columnId];
+      const pairedColumn = columns[doubleColumns[columnId]];
       return (
         <DoubleColumn
           key={`${columnId}-${doubleColumns[columnId]}-group`}
           column1={column}
           column2={pairedColumn}
           applicationCards1={column.applicationCardIds.map(
-            (taskId) => boardData.applicationCards[taskId]
+            (taskId) => applicationCards[taskId]
           )}
           applicationCards2={pairedColumn.applicationCardIds.map(
-            (taskId) => boardData.applicationCards[taskId]
+            (taskId) => applicationCards[taskId]
           )}
         />
       );
-    } else if (!Object.values(doubleColumns).includes(columnId)) {
-      const column = boardData.columns[columnId];
+    }
+
+    if (!Object.values(doubleColumns).includes(columnId)) {
+      const column = columns[columnId];
       return (
         <SingleColumn
           key={columnId}
           column={column}
           applicationCards={column.applicationCardIds.map(
-            (taskId) => boardData.applicationCards[taskId]
+            (taskId) => applicationCards[taskId]
           )}
         />
       );
     }
+
     return null;
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="flex flex-wrap p-4">
-        {boardData.columnOrder.map((columnId) => renderColumn(columnId))}
+        {columnOrder.map((columnId) => renderColumn(columnId))}
       </div>
     </DragDropContext>
   );
 };
-
 
 export default Board;
