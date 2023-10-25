50831033f54e1eda8824c848fe47400c852f7677
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2023-10-25 12:54:48 -0400

    clicking see more does not open modal

--- app/application-board/Description.tsx
+++ app/application-board/Description.tsx
@@ -4,15 +4,21 @@ const Description: React.FC<{
   description: string;
   isExpanded: boolean;
   toggle: () => void;
-}> = ({ description, isExpanded, toggle }) => (
-  <div className="mb-2 text-sm text-gray-500">
-    {isExpanded ? description : truncateText(description)}
-    {description?.length > MAX_CHARACTERS && (
-      <span className="text-blue-500 cursor-pointer" onClick={toggle}>
-        {isExpanded ? " see less" : " see more..."}
-      </span>
-    )}
-  </div>
-);
+}> = ({ description, isExpanded, toggle }) => {
+  const handleClick = (e: { stopPropagation: () => void }) => {
+    e.stopPropagation();
+    toggle();
+  };
 
+  return (
+    <div className="mb-2 text-sm text-gray-500">
+      {isExpanded ? description : truncateText(description)}
+      {description?.length > MAX_CHARACTERS && (
+        <span className="text-blue-500 cursor-pointer" onClick={handleClick}>
+          {isExpanded ? " see less" : " see more..."}
+        </span>
+      )}
+    </div>
+  );
+};
 export default Description;
